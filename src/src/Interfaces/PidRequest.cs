8916cc03570a3a7119eadc447201b882b5cf2384
dan parker <silverliingsw@hotmail.com>
2017-06-02 22:22:16 -0700

    Support multiframe messages (sort of). Also restructure the tick method to more evenly distribute load across updates.
    Related Work Items: #1

---
+++ src/src/Interfaces/PidRequest.cs
@@ -0,0 +1,41 @@
+﻿namespace DP.Tinast.Interfaces
+{
+    using System;
+
+    /// <summary>
+    /// Represent a PID request
+    /// </summary>
+    [Flags]
+    enum PidRequest
+    {
+        /// <summary>
+        /// The boost
+        /// </summary>
+        Boost = 0x0,
+
+        /// <summary>
+        /// The afr
+        /// </summary>
+        Afr = 0x1,
+
+        /// <summary>
+        /// The load
+        /// </summary>
+        Load = 0x2,
+
+        /// <summary>
+        /// The intake temp
+        /// </summary>
+        IntakeTemp = 0x4,
+
+        /// <summary>
+        /// The coolant temp
+        /// </summary>
+        CoolantTemp = 0x8,
+
+        /// <summary>
+        /// The oil temp
+        /// </summary>
+        OilTemp = 0x10
+    }
+}

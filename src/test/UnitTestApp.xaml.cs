1de14a7f00ba54222beb0ec094aa6d6c20c06275
Dan Parker <daparker@microsoft.com>
2017-07-26 13:21:23 -0700

    So basically the visual studio test runner doesn't work good with Xunit. So use the Xunit devices runner instead, which gives you a nice UI to launch tests from.
    Issue: 1

--- src/test/UnitTestApp.xaml.cs
+++ src/test/UnitTestApp.xaml.cs
@@ -2,8 +2,10 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.IO;
     using System.Linq;
+    using System.Reflection;
     using System.Runtime.InteropServices.WindowsRuntime;
     using Windows.ApplicationModel;
     using Windows.ApplicationModel.Activation;
@@ -18,12 +20,18 @@
     using Windows.UI.Xaml.Navigation;
     using MetroLog;
     using MetroLog.Targets;
+    using Xunit.Runners.UI;
 
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    sealed partial class App : Application
+    sealed partial class App : RunnerApplication
     {
+        /// <summary>
+        /// The log
+        /// </summary>
+        private ILogger log;
+
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -31,75 +39,30 @@
         public App()
         {
             this.InitializeComponent();
-            this.Suspending += OnSuspending;
+            this.UnhandledException += UnhandledExceptionHandler;
             LogManagerFactory.DefaultConfiguration.AddTarget(LogLevel.Debug, LogLevel.Fatal, new StreamingFileTarget());
+            this.log = LogManagerFactory.DefaultLogManager.GetLogger<App>();
         }
 
         /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
+        /// Called when the test runner initializes.
         /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        protected override void OnInitializeRunner()
         {
+            this.log.Info("Inside App.OnInitializeRunner");
 
-#if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                this.DebugSettings.EnableFrameRateCounter = true;
-            }
-#endif
-
-            Frame rootFrame = Window.Current.Content as Frame;
-
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
-
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
-            
-            Microsoft.VisualStudio.TestPlatform.TestExecutor.UnitTestClient.CreateDefaultUI();
-
-            // Ensure the current window is active
-            Window.Current.Activate();
-
-            Microsoft.VisualStudio.TestPlatform.TestExecutor.UnitTestClient.Run(e.Arguments);
+            // Make sure to declare dependent unit test classes here.
+            this.AddTestAssembly(typeof(TestTests).GetTypeInfo().Assembly);
         }
 
         /// <summary>
-        /// Invoked when Navigation to a certain page fails
+        /// The app specific unhandled exception handler.
         /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="UnhandledExceptionEventArgs"/> instance containing the event data.</param>
+        private void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
         {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
-
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
-            deferral.Complete();
+            this.log.Fatal("Unhandled exception in app", e.Exception);
         }
     }
 }
